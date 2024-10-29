ab7cde391f8e7ba333bca5e859f3dce1d25eef04
elisaniyogisubizo <e.niyogisub@alustudent.com>
2024-10-30 00:03:21 +0200

    basic

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-'''Task 8's module.
-'''
+""" 8. List all documents in Python
+"""
+
+import pymongo
 
 
 def list_all(mongo_collection):
-    '''Lists all documents in a collection.
-    '''
-    return [doc for doc in mongo_collection.find()]
+    """ list_all.
+    """
+    docs = mongo_collection.find()
+    return [x for x in docs]
--- NoSQL/README.md
+++ NoSQL/README.md
@@ -1,1 +1,1 @@
-# No SQL Module
+# NoSQL
